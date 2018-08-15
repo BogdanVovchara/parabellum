77182a7dae0186528964c008cba5241b27600429
Bohdan <you@example.com>
2018-08-16 01:12:57 +0300

    some

---
+++ bonus/index.php
@@ -0,0 +1,42 @@
+<?php require_once('header.php');?>
+            <section id="about_us">
+                <div class="bg-img minha">
+                    <p class="like-h2">About Us</p>
+                    <span class="hline"></span>
+                    <p class="head agam">Ultimate capability. Calm in adversity.</p>
+                </div>
+                <div class="bottom-block">
+                    <p class="head">Parabellum offers tailored training programmes and unique, intelligent experiences to equip elite individuals with the skills, intuition and preparation they require to function professionally at the highest level.</p>
+                    <p>Founded by members of Israeli intelligence communities and elite special forces, Parabellum’s philosophy and services are based on advanced knowledge of espionage, survival, anti-terrorism, hostage negotiation, combat and psychological warfare methods.</p>
+                    <p>The Parabellum experience is designed to challenge physically, emotionally and intellectually. Tailor-made to each client’s capabilities and resulting in better focus, preparation and ability to deal with extreme stress and unconventional, highly-charged situations</p>
+                </div>
+                <video src="video/video.mp4" controls loop muted autoplay>
+                    <br>
+                </video>
+            </section>
+            <!--  -->
+            <section id="espionage_challenge">
+                <div class="bg-img">
+                    <p class="like-h2">Espionage challenge</p>
+                    <span class="hline"></span>
+                    <p class="head">Challenge 0001/2018?</p>
+                </div>
+                <div class="bottom-block">
+                    <p class="head">You are undercover. </p>
+                    <p>Your mission is conducted under real-life conditions. Tailored to your capabilities and personal profile, our Espionage experience challenges you across all areas and stretches the limits of both your skill set and your imagination.</p>
+                </div>
+            </section>
+            <!--  -->
+            <section id="desert_challenge">
+                <div class="bg-img">
+                    <p class="like-h2">Desert challenge</p>
+                    <span class="hline"></span>
+                    <p class="head">Challenge 0002/2018?</p>
+                </div>
+                <div class="bottom-block">
+                    <p class="head">Dropped into desert conditions, this is a mission where preparation is key.</p>
+                    <p>
+                        Combining a variety of techniques and real-life circumstances, you will need to understand your survival skills and capabilities to escape in order to save your life, and others. As you are pursued by elite special forces veterans you will encounter extreme events and deal with uncertain situations and a loss of control in order to learn mental and physical techniques for you to remain in control.</p>
+                </div>
+            </section>
+<?php require_once('footer.php');?>
